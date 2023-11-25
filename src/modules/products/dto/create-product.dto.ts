38e41f5c6f1e2207253d5dd1f97bec4f4da777d4
xTheM4ST3Rx <them4sterdeveloper@outlook.com>
2023-11-25 13:44:45 -0300

    first commit

---
+++ src/modules/products/dto/create-product.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateProductDto {
+  @ApiProperty()
+  name: string;
+  @ApiProperty()
+  description: string;
+  @ApiProperty()
+  price: number;
+  @ApiProperty()
+  stock: number;
+  @ApiProperty()
+  image: string;
+}
---
+++ src/modules/products/products.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { PrismaService } from 'src/database/PrismaService';
+
+@Injectable()
+export class ProductsService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createProductDto: CreateProductDto) {
+    return this.prisma.product.create({
+      data: createProductDto,
+    });
+  }
+
+  findAll() {
+    return this.prisma.product.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.product.findUnique({
+      where: { id },
+    });
+  }
+
+  // update(id: number, updateProductDto: UpdateProductDto) {
+  //   return `This action updates a #${id} product`;
+  // }
+
+  remove(id: number) {
+    return this.prisma.product.delete({
+      where: { id },
+    });
+  }
+}
